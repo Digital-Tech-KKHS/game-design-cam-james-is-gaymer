eb0c293934c5e0b04c645b8f31be398c18720b52
LordCam2005 <81117319+LordCam2005@users.noreply.github.com>
2022-05-05 13:15:31 +1200

    started adding meteor sprites

--- views/entity.py
+++ views/entity.py
@@ -1,6 +1,7 @@
 import arcade
 
-SCALEING = 2
+ENEMY_SCALEING = 2
+DEBRIS_SCALING = 1
 
 
 class Entity(arcade.Sprite):
@@ -8,14 +9,32 @@ class Entity(arcade.Sprite):
 
         main_path = f"assets/{name_file}_idle.png"
         super().__init__(main_path)
-        self.scale = SCALEING
+
 
 
 class Enemy(Entity):
     def __init__(self, name_file):
         super().__init__(name_file)
+        self.scale = ENEMY_SCALEING
 
 
 class BasicEnemy(Enemy):
     def __init__(self, name_file):
         super().__init__("enemy")
+
+class Debris(Entity):
+    def __init__(self, name_file):
+        super().__init__(name_file)
+        self.scale = DEBRIS_SCALING
+
+class Scrap(Debris):
+    def __init__(self, name_file):
+        super().__init__(name_file)
+
+class Rock(Debris):
+    def __init__(self, name_file):
+        super().__init__(name_file)
+
+class Rocket(Scrap):
+    def __init__(self, name_file):
+        super().__init__("rocket")
