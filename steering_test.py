c9175e76c1169eed9f00eb8c9a82329c865ecca4
LordCam2005 <cameronAcampbell@icloud.com>
2022-06-10 19:01:49 +1200

    implemented physics engine

--- steering_test.py
+++ steering_test.py
@@ -4,6 +4,14 @@ import random
 import arcade
 from pyglet.math import Vec2
 
+PLAYER_ACCELERATION = 6000
+PLAYER_DEACCELERATION = 0.02
+PLAYER_MASS = 20
+PLAYER_FRICTION = 0.2
+PLAYER_MAX_SPEED = 215
+PLAYER_DAMPNING = 0.58
+
+DEFAULT_DAMPNING = 1.0
 
 class Vehicle(arcade.Sprite):
     def __init__(self, pos):
@@ -47,13 +55,36 @@ class Game(arcade.Window):
         super().__init__()
         arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
 
+        self.physics_engine = None
+
     def setup(self):
 
         self.scene = arcade.Scene()
         self.scene.add_sprite_list("vehicle")
+
+        self.physics_engine = arcade.PymunkPhysicsEngine(
+            damping=DEFAULT_DAMPNING, gravity=(0, 0)
+        )
+
         for i in range(50):
             self.vehicle = Vehicle(Vec2())
             self.scene.add_sprite("vehicle", self.vehicle)
+            self.physics_engine.add_sprite(
+            self.vehicle,
+            mass=PLAYER_MASS,
+            friction=PLAYER_FRICTION,
+            elasticity=0.4,
+            moment_of_inertia=0.8,
+            max_horizontal_velocity=PLAYER_MAX_SPEED,
+            max_vertical_velocity=PLAYER_MAX_SPEED,
+            damping=PLAYER_DAMPNING,
+        )
+
+        self.vehicle_body = self.physics_engine.get_physics_object(
+            self.vehicle
+        ).body
+
+        
 
     def on_draw(self):
         self.clear()
