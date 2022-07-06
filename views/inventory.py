3bd0312a8e4a590a6b4b95f0aafe403a9b187627
Cam <ccampbell@stu.kerikerihigh.ac.nz>
2022-07-06 12:14:44 +1200

    inventory grid is drawn and centered

--- views/inventory.py
+++ views/inventory.py
@@ -1,13 +1,24 @@
 import arcade
-
+from pyglet.math import Vec2
+from const import *
 
 class InventoryView(arcade.View):
     def __init__(self, window: arcade.Window = None):
         super().__init__(window)
 
+        self.inventory_grid = None
+        self.camera = arcade.Camera()
+    
+    def on_show(self):
+        self.inventory_grid = arcade.Sprite("assets\inventory_grid.png", 5)
+        self.inventory_grid.center_x = WIDTH/2
+        self.inventory_grid.center_y = HEIGHT/2
+        
+
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_rectangle_filled(0,0,200,200,arcade.color.WHITE)
+        self.camera.use()
+        self.inventory_grid.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.E:
