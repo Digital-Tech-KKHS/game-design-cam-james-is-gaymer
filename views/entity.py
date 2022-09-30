b50f71b2c7371286fc927a04a550b4aa826c90de
LordCam2005 <81117319+LordCam2005@users.noreply.github.com>
2022-09-30 14:00:49 +1300

    comments

--- views/entity.py
+++ views/entity.py
@@ -106,6 +106,7 @@ class Enemy(Vehicle):
         self.scale = ENEMY_SCALEING
         self.enemy_texures = []
         self.odo = 0
+        # loads animation 
         for i in range(4):            
             self.enemy_texures.append(arcade.load_texture(f"assets/{name_file}{i + 1}.png"))
     
@@ -113,7 +114,7 @@ class Enemy(Vehicle):
         self.odo += 1
         if self.odo < 10:
             return 
-            
+
         self.odo = 0
         self.cur_texture += 1
         if self.cur_texture > 3:
